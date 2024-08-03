99c321c4262af31d747aa95c0993a36f9796d87a
nikhil-03 <nikhilprakash2012@gmail.com>
2024-08-04 00:24:04 +0530

    doctor patient and appointment apis done, groomings/dto left

---
+++ src/main/java/com/nik/doctor/services/Configuration/Webconfig.java
@@ -0,0 +1,19 @@
+package com.nik.doctor.services.Configuration;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Webconfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
+                .allowedHeaders("*"); // Allow all headers
+
+    }
+}
